a3c499c8d8532e597e9543dcda39401eecdfa799
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-05-01 14:03:09 +0530

    Update metadata.py

--- plugins/metadata.py
+++ plugins/metadata.py
@@ -13,6 +13,24 @@ async def update_video_metadata(user_id, metadata_info):
 
 # Your existing code for handling metadata commands
 # ...
+ON = [[InlineKeyboardButton('Enabled ✅', callback_data='metadata_1')], [
+    InlineKeyboardButton('Set Metadata', callback_data='custom_metadata')]]
+OFF = [[InlineKeyboardButton('Disabled ❌', callback_data='metadata_0')], [
+    InlineKeyboardButton('Set Metadata', callback_data='custom_metadata')]]
+
+
+@Client.on_message(filters.private & filters.command('metadata'))
+async def handle_metadata(bot: Client, message: Message):
+
+    ms = await message.reply_text("**Please Wait...**", reply_to_message_id=message.id)
+    bool_metadata = await db.get_metadata(message.from_user.id)
+    user_metadata = await db.get_metadata_code(message.from_user.id)
+    await ms.delete()
+    if bool_metadata:
+
+        return await message.reply_text(f"Your Current Metadata:-\n\n➜ `{metadata_info}` ", reply_markup=InlineKeyboardMarkup(ON))
+
+    return await message.reply_text(f"Your Current Metadata:-\n\n➜ `{metadata_info}` ", reply_markup=InlineKeyboardMarkup(OFF))
 
 @Client.on_callback_query(filters.regex('.*?(custom_metadata|metadata).*?'))
 async def query_metadata(bot: Client, query: CallbackQuery):
@@ -29,7 +47,7 @@ async def query_metadata(bot: Client, query: CallbackQuery):
             await db.set_metadata(query.from_user.id, bool_meta=True)
             await query.message.edit(f"Your Current Metadata:-\n\n➜ `{user_metadata}` ", reply_markup=InlineKeyboardMarkup(ON))
 
-    elif data == 'cutom_metadata':
+    elif data == 'custom_metadata':
         await query.message.delete()
         try:
             # Ask user for video title
@@ -70,6 +88,6 @@ async def query_metadata(bot: Client, query: CallbackQuery):
 
             ms = await query.message.reply_text("**Please Wait...**", reply_to_message_id=metadata.id)
             await db.set_metadata_code(query.from_user.id, metadata_code=metadata.text)
-            await ms.edit("**Your Metadata Code Set Successfully ✅**")
+            await ms.edit("**Your Metadata Code Successfully Added ✅**")
         except Exception as e:
             print(e)
